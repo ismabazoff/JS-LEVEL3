d1aaa2697bac3900f55d6ee9f92fa2984ad586f6
Ismail Abazov <41200208+ismabazoff@users.noreply.github.com>
2023-12-05 15:11:34 +0300

    Delete src/api directory

--- src/api/route-api.ts
+++
@@ -1,9 +0,0 @@
-import {BaseApi} from "./base-api";
-
-const baseApi = new BaseApi()
-class RouteApi {
-    public navigatePages (url: string, data: any) {
-        return baseApi.post({url, data});
-    }
-}
-export default RouteApi;
